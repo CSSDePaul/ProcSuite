50b74e801b0d6aff995a46a866c131c43e5c3a7f
Li-Wey Lu <liweylu@gmail.com>
2016-04-09 19:00:04 -0600

    Update

--- ProcFreeze.py
+++ ProcFreeze.py
@@ -1,12 +1,20 @@
 from win32com.client import DispatchWithEvents
 from Tkinter import Tk
 
-whitelist = []
-
 class handler:
+    def __init__(self):
+        self.whitelistAccount = ["Administrator"]
+        self.whitelistProcess = ["csrss.exe","winlogon.exe","LogonUI.exe"]
+        self.bypassProcess = ["rdpclip.exe","dwm.exe"]
+        self.prevProcess = []
+        
     def OnProcessStarted(self, process):
-        if process.UserName not in whitelist:
+        if process.UserName not in self.whitelistAccount and process.Name not in self.whitelistProcess and not len(set(self.bypassProcess)&set(self.prevProcess)):
             process.Terminate()
+        self.prevProcess.append(process.Name)
+
+    def OnProcessTerminated(self, process):
+        self.prevProcess = []
       
 manager = DispatchWithEvents('DeviareCOM.NktSpyMgr', handler)
 manager.Initialize()
