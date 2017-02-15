8725b69cc386d1a664eac2b86bbe56d70db3db5d
Li-Wey Lu <liweylu@gmail.com>
2017-02-15 17:41:46 -0600

    Update

--- ProcFreeze.py
+++ ProcFreeze.py
@@ -5,13 +5,10 @@ class handler:
     def __init__(self):
         self.whitelistAccount = ["BackupAdministrator"]
         self.whitelistProcess = ["csrss.exe","winlogon.exe","LogonUI.exe","explorer.exe"]
-        self.bypassProcess = []
-        self.prevProcess = []
         
     def OnProcessStarted(self, process):
-        if process.UserName not in self.whitelistAccount and process.Name not in self.whitelistProcess and not len(set(self.bypassProcess)&set(self.prevProcess)):
+        if process.UserName not in self.whitelistAccount and process.Name not in self.whitelistProcess:
             process.Terminate()
-        self.prevProcess.append(process.Name)
 
     def OnProcessTerminated(self, process):
         self.prevProcess = []
